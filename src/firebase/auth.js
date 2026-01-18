032243fcae99f063af8106e786280a0288461174
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-01-18 18:29:22 +0530

    live

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,8 +1,9 @@
 import { auth, db } from "./firebase";
-import { doc, setDoc } from "firebase/firestore";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 
-export async function createUser(name, email, password) {
+// REGISTER
+export async function registerUser(name, email, password) {
   try {
     const userCred = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCred.user;
@@ -12,15 +13,18 @@ export async function createUser(name, email, password) {
       email,
       wishlist: [],
       teachSkills: [],
+      skillAcquired: [],
+      activeCourses: [],
       createdAt: new Date()
     });
 
-    return { success: true };
+    return { success: true, user };
   } catch (err) {
     return { success: false, error: err.message };
   }
 }
 
+// LOGIN
 export async function loginUser(email, password) {
   try {
     await signInWithEmailAndPassword(auth, email, password);
@@ -28,4 +32,12 @@ export async function loginUser(email, password) {
   } catch (err) {
     return { success: false, error: err.message };
   }
+}
+
+// GET USER DATA
+export async function getUserData(uid) {
+  const snap = await getDoc(doc(db, "users", uid));
+  
+  if (!snap.exists()) return null;
+  return snap.data();
 }
