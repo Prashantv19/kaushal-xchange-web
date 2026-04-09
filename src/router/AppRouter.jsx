6c346cdccce67392c355a1a051db6f94faf0669f
Prashant Vishwakarma <prashantv1902@gmail.com>
2026-04-09 23:57:32 +0530

    updated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -7,8 +7,6 @@ import TeachModule from "../pages/TeachModule";
 import Register from "../pages/auth/Register";
 import Login from "../pages/auth/Login";
 
-
-
 export default function AppRouter() {
   return (
     <BrowserRouter>
